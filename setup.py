944cdb77cc98ec09f5af2d24f5c2e4bf7f61343a
Rahul-404 <rahulshelke981@gmail.com>
2025-05-25 19:09:51 +0530

    Implement data ingestion functionality and update requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="Rahul Shelke",
+    author_email="rahulshelke981@gmail.com",
+    packages=find_packages(),
+    install_requires=[], 
+)
+
